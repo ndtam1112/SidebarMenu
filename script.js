46206e3cc7e10bb82d787f7de23df1e8cc2c34e6
ndtam1112 <ndtam1010@gmail.com>
2022-11-16 03:23:01 +0700

    .

---
+++ script.js
@@ -0,0 +1,18 @@
+let list = document.querySelectorAll('.list');
+
+let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+menuToggle.onclick = ()=>{
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active')
+}
+
+for (let i=0; i<list.length; i++) {
+    list[i].onclick = ()=>{
+        let j = 0;
+        while(j < list.length){
+            list[j++].className = 'list';
+        }
+        list[i].className = 'list active';
+    }
+}
